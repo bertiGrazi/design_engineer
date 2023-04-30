d450fdf05a9ded73150e3159ae025e05541a985c
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-04-30 20:45:13 -0300

    applying css style

--- exercicios/pages/basicos/propsSomenteLeitura.jsx
+++ exercicios/pages/basicos/propsSomenteLeitura.jsx
@@ -2,6 +2,7 @@ import SomaUm from "@/components/SomaUm";
 
 export default function propsSomenteLeitura() {
   return (
+    //TypeError: Cannot assign to read only property 'numero' of object '#<Object>'
     <SomaUm  numero={99}/>
   )
 }
