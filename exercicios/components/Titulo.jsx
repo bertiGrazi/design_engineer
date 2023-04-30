5a0a10b7a25910ddf20b526c31d4dd4fc236d83a
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-04-30 19:48:04 -0300

    learning about props

--- exercicios/components/Titulo.jsx
+++ exercicios/components/Titulo.jsx
@@ -1,8 +1,35 @@
-export default function Titulo() {
-  return (
+export default function Titulo(props) {
+  // props = propriedades que são passadas para um component 
+  console.log(props)
+  /*
+   if (props.pequeno) {
+      return (
+        <>
+          <p>São Paulo Futebol Clube</p>
+        </>
+      )
+   } else {
+      return (
+        <>
+        <h1>{props.principal}</h1>
+        <h2>{props.secundario}</h2>
+        <h3>{props.teste}</h3>
+        <h4>{props.grazi}</h4>
+      </>
+      )
+   }
+   */
+  // if ternário
+  return props.pequeno ? (
     <>
-      <h1>Título</h1>
-      <h2>Subtítulo</h2>
+      <p>São Paulo Futebol Clube</p>
+    </>
+  ) : (
+    <>
+      <h1>{props.principal}</h1>
+      <h2>{props.secundario}</h2>
+      <h3>{props.teste}</h3>
+      <h4>{props.grazi}</h4>
     </>
   )
 }
--- exercicios/pages/basicos/usandoTitulo.jsx
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,13 @@ import Titulo from '../../components/Titulo'
 export default function usandoTitulo() {
   return (
     <div>
-     <Titulo />
+     <Titulo 
+      principal = "Pagina de Cadastro"
+      secundario = "Incluir, alterar e excluir coisas!"
+      teste = "123"
+      grazi = "Grazielli"
+      pequeno = {true}
+     />
     </div>
   )
 }
