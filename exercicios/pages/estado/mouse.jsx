0ddd611a6bf552829f28df999a684da5d2667d00
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-05-04 09:38:58 -0300

    just create element

---
+++ exercicios/pages/estado/mouse.jsx
@@ -0,0 +1,18 @@
+export default function mouse() {
+  return (
+    <div style={{
+      display:"flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "pink",
+      color: "black",
+      fontWeight: "bold"
+      
+    }}>
+      <span>Eixo X: ?</span>
+      <span>Eixo Y: ?</span>
+    </div>
+  )
+}
